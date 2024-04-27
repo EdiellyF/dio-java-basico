ceadf8bd8e6b93f2ed911e29bb9f272036579c73
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2024-04-27 08:20:18 -0300

    classes e variaveis

--- java-anatomia/src/MinhaClasse.java
+++ java-anatomia/src/MinhaClasse.java
@@ -1,8 +1,20 @@
 public class MinhaClasse {
     
  public static void main(String[] args) {
-    System.out.print("oi! Seja bem vindos :)");
+   
+   String primeiroNome = "Edielly";
+   String segundoNome = "Ferreira";
 
+   String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+   System.out.println(nomeCompleto);
+ }
+
+
+ public static String nomeCompleto(String primeiroNome, String segundoNome){
+      return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
  }
 
 }
+
+//a classe deve possuir o mesmo nome dp arquivo.java
+//toda variavel deve ser escrita com letra minúscula
