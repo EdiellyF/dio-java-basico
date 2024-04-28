93cc3f0095da365c922279905d016ce775c441ea
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2024-04-28 16:04:40 -0300

    Tipos-de-Variaveis

---
+++ variaveis-trilha/tipos/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+       // TiposEVariaveis.java 
+       //classe string 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+}
